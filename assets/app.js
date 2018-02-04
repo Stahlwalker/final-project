4e3169cfc6eadf532ff0aebefb985b7a01c89fa4
Luke Stahl <Stahl1109@gmail.com>
2018-02-04 12:24:47 -0600

    style and basic html foundation commit

--- assets/app.js
+++ assets/app.js
@@ -262,3 +262,13 @@ $("#add-beer").on("click", function (event) {
     $("#location").val("");
     $("#your-thoughts").val("");
 });
+
+    // Get the modal
+    var modal = document.getElementById('id01');
+          
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
