78b007d5137385160660214f6c53856ecdf77437
Luke Stahl <Stahl1109@gmail.com>
2018-02-07 11:02:13 -0600

    fixed

--- assets/app.js
+++ assets/app.js
@@ -19,7 +19,7 @@ var uid;
 
 const txtEmail = document.getElementById('txtEmail');
 const txtPassword = document.getElementById('txtPassword');
-const txtPassword = document.getElementById('txtPassword2');
+const txtPassword2 = document.getElementById('txtPassword2');
 const btnLogin = document.getElementById('btnLogin');
 const btnSignUp = document.getElementById('btnSignUp');
 btnLogin.addEventListener('click', e => {
